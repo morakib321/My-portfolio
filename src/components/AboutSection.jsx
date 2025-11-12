5e34dfdc9e8b9447e38f575b303c73901cc5f065
Mohammed Rakib <124345592+morakib321@users.noreply.github.com>
2025-11-11 22:10:03 -0500

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,6 +34,7 @@ export const AboutSection = () => {
 
               <a
                 href="/projects/Mohammed_Rakibul_Hossain_Web_Developer.pdf"
+                download="Mohammed_Rakibul_Hossain_Web_Developer.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
